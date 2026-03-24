af78e76b4b72a02318b41a0815db6f07ccdba92b
Felipe Dornelles <felipedornellesbrasil@outlook.com>
2026-03-24 19:43:58 -0300

    Add files via upload

---
+++ adulto/release/adulto_003.py
@@ -0,0 +1,31 @@
+"""
+Programa adulto
+Descrição: Este programa pergunta a idade de uma pessoa.
+Se a idade for maior do que 18 anos, o programa imprime
+na tela o texto "Oi! Você e um adulto."
+Autor: Nelson
+Data: 24/03/2026
+Versão: 0.0.1
+"""
+
+# Alocação de memória
+
+idade = 0
+texto = ""
+
+# Entrada de dados
+
+idade = int(input("\nQual é a sua idade?"))
+
+# Processamento de dados
+
+if idade >= 18:
+    texto = "Oi! Você é um adulto."
+elif idade >= 14 < 18:
+    texto = "Você é um adolescente! Segura firme!!!"
+else:
+    texto = "Você é uma criança! Suma!!!"
+
+    # Saída de dados
+
+print(texto)
